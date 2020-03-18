53568977ed5ef877544d42e569168fff9ed14f71
Charles Pelton <charliejp0311@gmail.com>
2020-03-18 18:22:37 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -2,5 +2,8 @@ CREATE TABLE bears(
   id TYPE INTEGER PRIMARY KEY,
   name TYPE TEXT,
   age TYPE INTEGER,
-
-)
+  gender TYPE TEXT,
+  color TYPE TEXT,
+  temperament TYPE TEXT,
+  alive TYPE BOOLEAN
+);
