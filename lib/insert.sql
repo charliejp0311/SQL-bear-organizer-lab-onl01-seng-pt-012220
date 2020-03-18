551637e32ba9d7782a973e89372a5c79c76067f9
Charles Pelton <charliejp0311@gmail.com>
2020-03-18 19:21:19 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 5, "Male", "Chocolate", "Angree", true);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ('Rowdy', 3, "Male", "Plad", "Scotish", true);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ('Tabitha', 1, "Female", "Chocolate", "Imature", true);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 9, "Female", "Brown", "Astute", true);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ('Melissa', 4, "Female", "Black", "Lover", true);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ('Grinch', 250, "Male", "Green", "Grouch", true);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ('Wendy', 7, "Female", "Chocolate", "Fast food lover", true);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 5, "Male", "Chocolate", "Angree", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ('Rowdy', 3, "Male", "Plad", "Scotish", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ('Tabitha', 1, "Female", "Chocolate", "Imature", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 9, "Female", "Brown", "Astute", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ('Melissa', 4, "Female", "Black", "Lover", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ('Grinch', 250, "Male", "Green", "Grouch", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ('Wendy', 7, "Female", "Chocolate", "Fast food lover", 1);
 INSERT INTO bears(name, age, gender, color, temperament) VALUES (NULL, 5, "Male", "Chocolate", "Angree");
