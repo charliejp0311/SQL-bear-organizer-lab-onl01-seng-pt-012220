ca22c86c182194a976d2cfcb9552f420498265b4
Charles Pelton <charliejp0311@gmail.com>
2020-03-18 19:19:03 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears(name, age, gender, color, temperament) VALUES ('Mr. Chocolate', 5, "Male", "Chocolate", "Angree");
-INSERT INTO bears(name, age, gender, color, temperament) VALUES ('Rowdy', 3, "Male", "Plad", "Scotish");
-INSERT INTO bears(name, age, gender, color, temperament) VALUES ('Tabitha', 1, "Female", "Chocolate", "Imature");
-INSERT INTO bears(name, age, gender, color, temperament) VALUES ('Sergeant Brown', 9, "Female", "Brown", "Astute");
-INSERT INTO bears(name, age, gender, color, temperament) VALUES ('Melissa', 4, "Female", "Black", "Lover");
-INSERT INTO bears(name, age, gender, color, temperament) VALUES ('Grinch', 250, "Male", "Green", "Grouch");
-INSERT INTO bears(name, age, gender, color, temperament) VALUES ('Wendy', 7, "Female", "Chocolate", "Fast food lover");
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 5, "Male", "Chocolate", "Angree", true);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ('Rowdy', 3, "Male", "Plad", "Scotish", true);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ('Tabitha', 1, "Female", "Chocolate", "Imature", true);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 9, "Female", "Brown", "Astute", true);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ('Melissa', 4, "Female", "Black", "Lover", true);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ('Grinch', 250, "Male", "Green", "Grouch", true);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ('Wendy', 7, "Female", "Chocolate", "Fast food lover", true);
 INSERT INTO bears(name, age, gender, color, temperament) VALUES (NULL, 5, "Male", "Chocolate", "Angree");
