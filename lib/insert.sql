3003f786d47a6bb8a6f699e3ba73b69902a18a10
Charles Pelton <charliejp0311@gmail.com>
2020-03-18 18:49:25 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,8 @@
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 5, "Male", "Chocolate", "Angree", FALSE);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 5, "Male", "Chocolate", "Angree", TRUE);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ('Rowdy', 3, "Male", "Plad", "Scotish", TRUE);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ('Tabitha', 1, "Female", "Chocolate", "Imature", TRUE);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 9, "Female", "Brown", "Astute", TRUE);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ('Melissa', 4, "Female", "Black", "Lover", TRUE);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ('Grinch', 5, "Male", "Chocolate", "Grouch", TRUE);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 5, "Male", "Chocolate", "Angree", TRUE);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES (NULL, 5, "Male", "Chocolate", "Angree", FALSE);
